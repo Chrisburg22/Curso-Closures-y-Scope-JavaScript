2bb75ae41415e3b06ec755a6562395d35885bbd8
Christian Alejandro Ramos Pérez <94751604+Chrisburg22@users.noreply.github.com>
2022-06-24 18:35:49 -0500

    Create strict.js
    El modo estricto vuelve a JavaScript a un lenguaje estricto quitándole su peculiaridad de reparar los errores de sintaxis

---
+++ src/scope/strict.js
@@ -0,0 +1,16 @@
+
+function imprimePi (){
+    "use strict";
+    return pi = 3.1416;
+}
+console.log(imprimePi());
+
+function imprimePiStrict(){
+    'use strict';
+    let pi = 3.1416;
+    return pi; 
+}   
+ console.log(imprimePiStrict());
+
+ pi =3.1416;
+ console.log(pi);
